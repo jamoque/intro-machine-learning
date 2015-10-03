1d89d0b88a4b1e09636885bf02f8ea44454af33a
Jeremy Hintz <jeremy.hintz@gmail.com>
2015-10-02 23:10:43 -0700

    mini-project 2: support vector machines

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,6 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel='rbf', C=10000.0)
+print "Training SVM..."
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training completed in: ", round(time()-t0, 3), "sec"
+print "Making predictions..."
+t0 = time()
+pred = clf.predict(features_test)
+print "Predictions made in: ", round(time()-t0, 3), "sec"
+accuracy = accuracy_score(labels_test, pred)
+print "Accuracy of predictions: " + str(round(accuracy, 3) * 100) + "%"
 
 #########################################################
 
